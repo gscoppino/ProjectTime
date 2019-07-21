ca034fcf467edba857e6219e940f6ad553a04afe
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-07-21 11:04:22 -0400

    Use annotation/aggregation to compute charge time where possible.

---
+++ project/querysets.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class ChargeQuerySet(models.QuerySet):
+    def annotate_time_charged(self):
+        return self.annotate(
+            db__time_charged=models.F('end_time') - models.F('start_time')
+        )
+
+    def aggregate_time_charged(self):
+        return self.annotate_time_charged().aggregate(
+            total_time_charged=models.Sum('db__time_charged')
+        ).get('total_time_charged')
