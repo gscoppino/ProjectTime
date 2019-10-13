e7c75fbd52c438ebfbd7cc57e68ebef453636a2b
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-10-13 12:10:20 -0400

    Shows the current timezone in the Django admin top bar.

--- src/project/site.py
+++ src/project/site.py
@@ -12,5 +12,16 @@ class ProjectTimeAdminSite(AdminSiteDefaultFilterMixin, admin.AdminSite):
         'project.Charge': DEFAULT_CHARGE_CHANGELIST_FILTERS
     }
 
+    def each_context(self, request):
+        context = super().each_context(request)
+
+        if context.get('timezone') is not None:
+            raise ValueError('Timezone context key would conflict with an'
+                             'existing key in the Django admin context.')
+
+        context['timezone'] = request.session.get('timezone') or 'N/A'
+
+        return context
+
 
 admin_site = ProjectTimeAdminSite()
