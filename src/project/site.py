6e3ae0e6c6e949bcfd1b2e32fab869c675b55fcb
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-10-13 20:12:41 -0400

    Adds a monthly summary dashboard view to the Django admin.

--- src/project/site.py
+++ src/project/site.py
@@ -1,6 +1,14 @@
+from calendar import monthrange
+from datetime import timedelta
 from django.contrib import admin
+from django.db.models import F, Sum
+from django.http import JsonResponse
+from django.template.response import TemplateResponse
+from django.utils import timezone
+from django.urls import path
 from .constants import DEFAULT_PROJECT_CHANGELIST_FILTERS, DEFAULT_CHARGE_CHANGELIST_FILTERS
 from .mixins import AdminSiteDefaultFilterMixin
+from .models import Project, Charge
 
 
 class ProjectTimeAdminSite(AdminSiteDefaultFilterMixin, admin.AdminSite):
@@ -22,6 +30,61 @@ class ProjectTimeAdminSite(AdminSiteDefaultFilterMixin, admin.AdminSite):
         context['timezone'] = request.session.get('timezone') or 'N/A'
 
         return context
+
+    def get_urls(self):
+        urls = super().get_urls()
+        extra_urls = [
+            path('dashboard',
+                 self.admin_view(self.dashboard_view),
+                 name='dashboard'),
+
+            path('monthly-summary', self.admin_view(self.monthly_summary_view))
+        ]
+
+        return extra_urls + urls
+
+    def dashboard_view(self, request):
+        context = {
+            **self.each_context(request),
+            'projects': Project.objects.all()
+        }
+
+        return TemplateResponse(request,
+                                "admin/dashboard.html",
+                                context)
+
+    def monthly_summary_view(self, request):
+        project_ids = request.GET.getlist('project')
+
+        queryset = Charge.objects.all()
+        if project_ids:
+            queryset = queryset.filter(project__in=project_ids)
+
+        today = timezone.localtime()
+        start_of_month, end_of_month = monthrange(today.year, today.month)
+        limit_day = timedelta(days=1, microseconds=-1)
+
+        return JsonResponse(list((queryset
+                                  .filter(start_time__range=(
+                                      today.replace(day=start_of_month,
+                                                    hour=0,
+                                                    minute=0,
+                                                    second=0,
+                                                    microsecond=0),
+                                      today.replace(day=end_of_month,
+                                                    hour=0,
+                                                    minute=0,
+                                                    second=0,
+                                                    microsecond=0) + limit_day
+                                  ))
+                                  .select_related('project')
+                                  .values('project')
+                                  .order_by('project_id')
+                                  .annotate(project_name=F('project__name'))
+                                  .annotate(total_time_charged=Sum(
+                                      F('end_time') - F('start_time')
+                                  )))),
+                            safe=False)
 
 
 admin_site = ProjectTimeAdminSite()
