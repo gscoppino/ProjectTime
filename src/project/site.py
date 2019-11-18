4fab8b8b1bfdfd2694d154862a64a242d55ce2c6
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-11-17 19:11:39 -0500

    Convert monthly summary visualization from D3 to Bokeh.

--- src/project/site.py
+++ src/project/site.py
@@ -1,11 +1,16 @@
+import pandas as pd
 from calendar import monthrange
 from datetime import timedelta
+from bokeh.embed import components
+from bokeh.palettes import Category20c
+from bokeh.plotting import figure
+from bokeh.transform import cumsum
 from django.contrib import admin
 from django.db.models import F, Sum
-from django.http import JsonResponse
 from django.template.response import TemplateResponse
 from django.utils import timezone
 from django.urls import path
+from math import pi
 from .constants import DEFAULT_PROJECT_CHANGELIST_FILTERS, DEFAULT_CHARGE_CHANGELIST_FILTERS
 from .mixins import AdminSiteDefaultFilterMixin
 from .models import Project, Charge
@@ -36,57 +41,112 @@ class ProjectTimeAdminSite(AdminSiteDefaultFilterMixin, admin.AdminSite):
         extra_urls = [
             path('dashboard',
                  self.admin_view(self.dashboard_view),
-                 name='dashboard'),
-
-            path('monthly-summary',
-                 self.admin_view(self.monthly_summary_view),
-                 name='dashboard-data')
+                 name='dashboard')
         ]
 
         return extra_urls + urls
 
-    def dashboard_view(self, request):
-        context = {
-            **self.each_context(request),
-            'projects': Project.objects.all()
-        }
-
-        return TemplateResponse(request,
-                                "admin/dashboard.html",
-                                context)
-
-    def monthly_summary_view(self, request):
-        project_ids = request.GET.getlist('project')
-
-        queryset = Charge.objects.all()
+    def get_monthly_summary_series(self, date, project_ids=[]):
+        charges = Charge.objects.all()
         if project_ids:
-            queryset = queryset.filter(project__in=project_ids)
+            charges = charges.filter(project__in=project_ids)
 
-        today = timezone.localtime()
-        start_of_month, end_of_month = monthrange(today.year, today.month)
+        _, end_of_month = monthrange(date.year, date.month)
         limit_day = timedelta(days=1, microseconds=-1)
 
-        return JsonResponse(list((queryset
-                                  .filter(start_time__range=(
-                                      today.replace(day=start_of_month,
-                                                    hour=0,
-                                                    minute=0,
-                                                    second=0,
-                                                    microsecond=0),
-                                      today.replace(day=end_of_month,
-                                                    hour=0,
-                                                    minute=0,
-                                                    second=0,
-                                                    microsecond=0) + limit_day
-                                  ))
-                                  .select_related('project')
-                                  .values('project')
-                                  .order_by('project_id')
-                                  .annotate(project_name=F('project__name'))
-                                  .annotate(total_time_charged=Sum(
-                                      F('end_time') - F('start_time')
-                                  )))),
-                            safe=False)
+        charges = (charges
+                   .filter(start_time__range=(
+                       date.replace(day=1,
+                                    hour=0,
+                                    minute=0,
+                                    second=0,
+                                    microsecond=0),
+                       date.replace(day=end_of_month,
+                                    hour=0,
+                                    minute=0,
+                                    second=0,
+                                    microsecond=0) + limit_day
+                   ))
+                   .select_related('project')
+                   .values('project')
+                   .order_by('project_id')
+                   .annotate(project_name=F('project__name'))
+                   .annotate(total_time_charged=Sum(
+                       F('end_time') - F('start_time')
+                   )))
+
+        chart_data = ({
+            charge['project_name']:
+            charge['total_time_charged'].total_seconds() / 3600
+            for charge in charges
+        })
+
+        series = (pd.Series(chart_data)
+                  .reset_index(name='value')
+                  .rename(columns={'index': 'charge'}))
+
+        series['angle'] = series['value'] / series['value'].sum() * (2 * pi)
+
+        category_count = len(chart_data)
+        if category_count <= 20:
+            series['color'] = (Category20c[len(chart_data)]
+                               if category_count >= 3
+                               else Category20c[3][0:category_count])
+
+        return series
+
+    def get_monthly_summary_chart_components(self, series):
+        chart = figure(title="Monthly Summary",
+                       toolbar_location=None,
+                       tools="hover",
+                       tooltips="@charge: @value hour(s)",
+                       x_range=(-0.5, 1.0))
+
+        chart.wedge(source=series,
+                    x=0,
+                    y=1,
+                    radius=0.4,
+                    start_angle=cumsum('angle', include_zero=True),
+                    end_angle=cumsum('angle'),
+                    line_color='white',
+                    fill_color='color',
+                    legend_field='charge')
+
+        chart.axis.axis_label = None
+        chart.axis.visible = False
+        chart.grid.grid_line_color = None
+
+        return components(chart)
+
+    def dashboard_view(self, request):
+        project_ids = request.GET.getlist('project')
+
+        projects = ([
+            {
+                **project,
+                'selected': not project_ids or str(project['id']) in project_ids
+            }
+            for project in Project.objects.values('id', 'name')
+        ])
+
+        series = self.get_monthly_summary_series(
+            timezone.localtime(),
+            project_ids)
+
+        script, div = self.get_monthly_summary_chart_components(series)
+
+        context = {
+            **self.each_context(request),
+            'projects': projects,
+            'chart_script': script,
+            'chart_div': div
+        }
+
+        return TemplateResponse(
+            request=request,
+            template="admin/dashboard.html",
+            context=context
+        )
 
 
 admin_site = ProjectTimeAdminSite()
--- src/project/tests/test_site_views.py
+++ src/project/tests/test_site_views.py
@@ -1,18 +1,27 @@
+import pandas as pd
 from datetime import timedelta
 from django.contrib.admin import AdminSite
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 from django.test import TestCase
-from django.utils.duration import duration_iso_string
 from django.utils import timezone
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from ..models import Project
+from ..site import ProjectTimeAdminSite
 from .utils.general import validate_and_save
 from .utils.charge import ChargeFactory
 
 
-def get_mock_admin_context(self, request):
-    return {'foo': 'bar'}
+def get_mock_admin_context():
+    return MagicMock(return_value={'foo': 'bar'})
+
+
+def get_mock_monthly_summary_dataframe():
+    return MagicMock(return_value=pd.DataFrame())
+
+
+def get_mock_monthly_summary_chart():
+    return MagicMock(return_value=('script', '<div></div>'))
 
 
 class AdminUserTestCase(TestCase):
@@ -37,31 +46,63 @@ class ProjectTimeAdminSiteDashboardTemplateViewTestCase(AdminUserTestCase):
         response = self.client.get(reverse('admin:dashboard'))
         self.assertEqual(response.status_code, 200)
 
-    @patch.object(AdminSite, 'each_context', new=get_mock_admin_context)
-    def test_all_admin_context_is_available_on_context(self):
+    @patch.object(AdminSite, 'each_context', new_callable=get_mock_admin_context)
+    def test_all_admin_context_is_available_on_context(self, mock_method):
         self.performLogin()
         response = self.client.get(reverse('admin:dashboard'))
         self.assertIn('foo', response.context)
         self.assertEqual(response.context['foo'], 'bar')
 
-    def test_all_projects_are_available_on_context(self):
+    def test_all_projects_are_selected_by_default(self):
+        project_a = validate_and_save(Project(name='Project A'))
+        project_b = validate_and_save(Project(name='Project B'))
+
+        self.performLogin()
+
+        response = self.client.get(reverse('admin:dashboard'))
+
+        self.assertEqual(response.context['projects'], [
+            {'id': project_a.pk, 'name': project_a.name, 'selected': True},
+            {'id': project_b.pk, 'name': project_b.name, 'selected': True}
+        ])
+
+    def test_correct_projects_are_selected(self):
+        project_a = validate_and_save(Project(name='Project A'))
+        project_b = validate_and_save(Project(name='Project B'))
+
+        self.performLogin()
+
+        response = self.client.get('{url}?project={project}'.format(
+            url=reverse('admin:dashboard'),
+            project=project_a.pk
+        ))
+
+        self.assertEqual(response.context['projects'], [
+            {'id': project_a.pk, 'name': project_a.name, 'selected': True},
+            {'id': project_b.pk, 'name': project_b.name, 'selected': False}
+        ])
+
+    @patch.object(
+        ProjectTimeAdminSite,
+        'get_monthly_summary_series',
+        new_callable=get_mock_monthly_summary_dataframe
+    )
+    @patch.object(
+        ProjectTimeAdminSite,
+        'get_monthly_summary_chart_components',
+        new_callable=get_mock_monthly_summary_chart
+    )
+    def test_visualization_creation_blackbox(self, get_mock_dataframe, get_mock_chart_components):
         self.performLogin()
         response = self.client.get(reverse('admin:dashboard'))
-        self.assertQuerysetEqual(response.context['projects'],
-                                 Project.objects.all())
+        self.assertTrue(get_mock_dataframe.called)
+        self.assertTrue(get_mock_chart_components.called)
+        self.assertIn('chart_script', response.context)
+        self.assertEqual(response.context['chart_script'], 'script')
+        self.assertIn('chart_div', response.context)
+        self.assertEqual(response.context['chart_div'], '<div></div>')
 
-
-class ProjectAdminSiteDashboardJsonViewTestCase(AdminUserTestCase):
-    def test_view_redirects_when_user_is_not_admin(self):
-        response = self.client.get(reverse('admin:dashboard-data'))
-        self.assertEqual(response.status_code, 302)
-
-    def test_view_is_ok_when_user_is_admin(self):
-        self.performLogin()
-        response = self.client.get(reverse('admin:dashboard-data'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_returns_projects_with_total_charges_for_current_month(self):
+    def test_visualization_creation_whitebox(self):
         project_a = validate_and_save(Project(name='Project A'))
         project_b = validate_and_save(Project(name='Project B'))
 
@@ -113,35 +154,57 @@ class ProjectAdminSiteDashboardJsonViewTestCase(AdminUserTestCase):
             charge_time=timedelta(hours=1)
         ))
 
-        self.performLogin()
+        site = ProjectTimeAdminSite()
 
         # Should return data on all projects
 
-        response = self.client.get(reverse('admin:dashboard-data'))
-        self.assertJSONEqual(response.content, [
-            {'project': project_a.pk,
-             'project_name': 'Project A',
-             'total_time_charged': duration_iso_string(
-                 sum(project_a_current_charges,
-                     timedelta()))},
-            {'project': project_b.pk,
-             'project_name': 'Project B',
-             'total_time_charged': duration_iso_string(
-                 sum(project_b_current_charges,
-                     timedelta()))}
-        ])
+        dataframe = site.get_monthly_summary_series(timezone.localtime())
+
+        self.assertIsInstance(dataframe, pd.DataFrame)
+        self.assertEqual(len(dataframe), 2)
+
+        self.assertIsInstance(dataframe['charge'], pd.Series)
+        self.assertIsInstance(dataframe['value'], pd.Series)
+        self.assertIsInstance(dataframe['angle'], pd.Series)
+        self.assertIsInstance(dataframe['color'], pd.Series)
+
+        project_a_dataframe = dataframe[dataframe.charge == project_a.name]
+        project_b_dataframe = dataframe[dataframe.charge == project_b.name]
+
+        self.assertFalse(project_a_dataframe.empty)
+        self.assertFalse(project_b_dataframe.empty)
+
+        self.assertEqual(project_a_dataframe.iloc[0].value, 8.0)
+        self.assertEqual(project_b_dataframe.iloc[0].value, 8.0)
+
+        self.assertIsInstance(project_a_dataframe.iloc[0].angle, float)
+        self.assertIsInstance(project_b_dataframe.iloc[0].angle, float)
+
+        self.assertIsInstance(project_a_dataframe.iloc[0].color, str)
+        self.assertIsInstance(project_b_dataframe.iloc[0].color, str)
+
+        div, chart = site.get_monthly_summary_chart_components(dataframe)
+        self.assertGreater(len(div), 0)
+        self.assertGreater(len(chart), 0)
 
         # Should return data on only the requested projects
 
-        response = self.client.get('{url}?project={project}'.format(
-            url=reverse('admin:dashboard-data'),
-            project=project_a.pk
-        ))
+        dataframe = site.get_monthly_summary_series(timezone.localtime(),
+                                                    project_ids=[project_a.pk])
 
-        self.assertJSONEqual(response.content, [
-            {'project': project_a.pk,
-             'project_name': 'Project A',
-             'total_time_charged': duration_iso_string(
-                 sum(project_a_current_charges,
-                     timedelta()))}
-        ])
+        self.assertIsInstance(dataframe, pd.DataFrame)
+        self.assertEqual(len(dataframe), 1)
+
+        self.assertIsInstance(dataframe['charge'], pd.Series)
+        self.assertIsInstance(dataframe['value'], pd.Series)
+        self.assertIsInstance(dataframe['angle'], pd.Series)
+        self.assertIsInstance(dataframe['color'], pd.Series)
+
+        self.assertEqual(dataframe.iloc[0].charge, project_a.name)
+        self.assertEqual(dataframe.iloc[0].value, 8.0)
+        self.assertIsInstance(dataframe.iloc[0].angle, float)
+        self.assertIsInstance(dataframe.iloc[0].color, str)
+
+        div, chart = site.get_monthly_summary_chart_components(dataframe)
+        self.assertGreater(len(div), 0)
+        self.assertGreater(len(chart), 0)
