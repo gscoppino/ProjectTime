e092db836a9eea1096e3a2c1a4aa18f0f4e035a9
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-11-27 15:16:25 -0500

    Configures TimezoneView's success URL in admin urlconf.

--- src/timezone/tests/test_views.py
+++ src/timezone/tests/test_views.py
@@ -1,38 +1,48 @@
 from django.shortcuts import reverse
-from django.test import TestCase
+from django.views.generic import FormView
+from django.test import TestCase, RequestFactory
+from django.views.generic.edit import FormMixin
 from timezone.forms import TimezoneForm
+from timezone.views import TimezoneView
+from unittest.mock import MagicMock, patch
 
-# Create your tests here.
+
+class ViewTestCase(TestCase):
+    def setup_class_based_view(self, view_class, request, *args, **kwargs):
+        """
+        Mimic ``as_view()``, but returns view instance.
+        Use this function to get view instances on which you can run unit tests,
+        by testing specific methods.
+        """
+        view = view_class()
+        view.request = request
+        view.args = args
+        view.kwargs = kwargs
+        return view
 
 
-class TimezoneViewTestCase(TestCase):
-    def test_view_is_ok(self):
-        response = self.client.get(reverse('admin:select-timezone'))
-        self.assertEqual(response.status_code, 200)
+class TimezoneViewTestCase(ViewTestCase):
+    def test_inherits_from_django_formview(self):
+        self.assertTrue(issubclass(TimezoneView, FormView))
 
     def test_timezone_form_template_is_used(self):
-        response = self.client.get(reverse('admin:select-timezone'))
-        self.assertTemplateUsed(response, 'timezone_form.html')
+        self.assertTrue(TimezoneView.template_name == 'timezone_form.html')
 
     def test_timezone_form_is_used(self):
-        response = self.client.get(reverse('admin:select-timezone'))
-        self.assertIsInstance(response.context['form'], TimezoneForm)
+        self.assertTrue(TimezoneView.form_class is TimezoneForm)
 
-    def test_set_timezone_for_session_on_successful_submit(self):
-        self.assertEqual('timezone' in self.client.session, False)
+    @patch.object(FormMixin, 'form_valid')
+    def test_set_timezone_for_session_on_successful_submit(self, mock_method):
+        request = RequestFactory().get('/foo/bar')
+        request.session = {}
 
-        self.client.post(reverse('admin:select-timezone'), {
+        view = self.setup_class_based_view(TimezoneView, request)
+
+        form = TimezoneForm(data={
             'timezone': 'America/New_York'
         })
 
-        self.assertEqual(self.client.session['timezone'], 'America/New_York')
+        form.full_clean()
 
-    def test_redirects_to_login_on_successful_submit(self):
-        response = self.client.post(reverse('admin:select-timezone'), {
-            'timezone': 'America/New_York'
-        })
-
-        self.assertRedirects(
-            response,
-            reverse('admin:index'),
-            fetch_redirect_response=False)
+        view.form_valid(form)
+        self.assertEqual(request.session['timezone'], 'America/New_York')
