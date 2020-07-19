36786c416764f3587cbab98ce1c33efa846f87ca
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2020-07-19 20:30:59 +0000

    The python request code does not appear to have been the issue...let's update the build env and hope for the best

--- test/libraries/ProjectTimeLibrary.py
+++ test/libraries/ProjectTimeLibrary.py
@@ -1,11 +1,9 @@
 from robot.api.deco import keyword
 from urllib import request
+from urllib.error import URLError
 
 class ProjectTimeLibrary:
     @keyword('Check Application Status At URL "${url}"')
     def check_application_status(self, url):
-        http_handler = request.HTTPHandler()
-        opener = request.build_opener(http_handler)
-        request.install_opener(opener)
         with request.urlopen(url) as f:
             f.getcode()
