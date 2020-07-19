726bcedc86ca05b0856ab77e4c2081b2fd8a8ff5
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2020-07-19 21:26:24 +0000

    Moving from Travis CI to Circle CI

--- test/libraries/ProjectTimeLibrary.py
+++ test/libraries/ProjectTimeLibrary.py
@@ -1,15 +1,8 @@
-import os
-import time
 from robot.api.deco import keyword
 from urllib import request
-from urllib.error import URLError
 
 class ProjectTimeLibrary:
     @keyword('Check Application Status At URL "${url}"')
     def check_application_status(self, url):
-        if os.environ.get("TRAVIS", None):
-            time.sleep(10)
-            return
-
         with request.urlopen(url) as f:
             f.getcode()
